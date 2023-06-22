688ce86c8cb539c638b8b6d819c3f6a4ee779ccc
Sveta Golubeva <veta.golubeva@gmail.com>
2023-06-22 23:10:19 +0300

    Run test

--- src/test/java/ru/netology/BankCardApplicationTest.java
+++ src/test/java/ru/netology/BankCardApplicationTest.java
@@ -1,3 +1,24 @@
 package ru.netology;
-public class BankCardApplicationTest {
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class BankCardApplicationTest {
+    @Test
+    void ShouldFillTheForm() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ромашка Полевая");
+        form.$("[data-test-id=phone] input").setValue("+79123456789");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
 }
