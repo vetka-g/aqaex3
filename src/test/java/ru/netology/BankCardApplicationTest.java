aad44c8e19417e816660202cd250a49ca345a3e1
Sveta Golubeva <veta.golubeva@gmail.com>
2023-06-23 10:35:12 +0300

    Phone broken

--- src/test/java/ru/netology/BankCardApplicationTest.java
+++ src/test/java/ru/netology/BankCardApplicationTest.java
@@ -30,7 +30,19 @@ class BankCardApplicationTest {
         form.$("[data-test-id=phone] input").setValue("+79123456789");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void ShouldFillThePhoneWithError() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ромашка Полевая");
+        form.$("[data-test-id=phone] input").setValue("+7912345678");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
 }
