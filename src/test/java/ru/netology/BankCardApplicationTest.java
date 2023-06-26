b93ab9da0246160f01959729876deea57904953f
Sveta Golubeva <veta.golubeva@gmail.com>
2023-06-26 18:30:20 +0300

    tests fixed

--- src/test/java/ru/netology/BankCardApplicationTest.java
+++ src/test/java/ru/netology/BankCardApplicationTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -10,9 +11,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class BankCardApplicationTest {
-    @Test
-    void ShouldFillTheForm() {
+
+    @BeforeEach
+    void setUp() {
         open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldFillTheFormCorrect() {
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Ромашка Полевая-Лечебная");
         form.$("[data-test-id=phone] input").setValue("+79123456789");
@@ -22,39 +28,58 @@ class BankCardApplicationTest {
 
     }
 
-   @Test
-    void ShouldFillTheFormWithError() {
-        open("http://localhost:9999/");
+    @Test
+    void shouldFillNameWithLatin() {
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Romashka Polevaya");
         form.$("[data-test-id=phone] input").setValue("+79123456789");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
 
     }
 
     @Test
-    void ShouldFillThePhoneWithError() {
-        open("http://localhost:9999/");
+    void shouldNotFillName() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79123456789");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void shouldFillThePhoneWithError() {
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Ромашка Полевая");
         form.$("[data-test-id=phone] input").setValue("+7912345678");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
 
     @Test
-    void ShouldNotCheckTheBox() {
-        open("http://localhost:9999/");
+    void shouldNotFillThePhone() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ромашка Полевая");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void shouldCheckTheBox() {
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Ромашка Полевая");
         form.$("[data-test-id=phone] input").setValue("+7912345678");
-        form.$("[data-test-id=agreement]");
+        form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=agreement]").shouldNotBe(selected);
+        $("[data-test-id=agreement]").shouldBe(visible);
 
     }
 }
